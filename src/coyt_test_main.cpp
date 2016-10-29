cc0cfb6d21d52d9ed6ad36e2ea5a3a83842f755f
coyt <coyt.barringer@Gmail.com>
2016-10-29 00:12:50 -0400

    shitsworking

--- src/coyt_test_main.cpp
+++ src/coyt_test_main.cpp
@@ -10,26 +10,42 @@ Coyt Barringer
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#   include <WiFiClientSecure.h>
+
+#include <ArduinoJson.h>
 
 #include <Adafruit_MCP9808.h>
 #include <Wire.h>
 
 #include "VFD.h"
+/*
+#define USE_SSL 1
+#if USE_SSL
 
+    WiFiClientSecure client;
+#   define PORT 443
+#else
+    WiFiClient client;
+#   define PORT 80
+#endif
+*/
 //constants -----
-const char WIFI_SSID[] = "AvalonHeights";
-const char WIFI_PSK[] = "";
+const char* ssid     = "HellSpot Slow"; //HellSpot Slow
+const char* password = "ILikeWiFi"; //ILikeWiFi
 
-const char http_site[] = "www.example.com";
-const int http_port = 80;
+const char* host = "api.coindesk.com";
+const char* hostEth = "api.nanopool.org";
 
 //Initialize Objects -----
-//SoftwareSerial mySerial(D5,D6);//rx,tx
 SoftwareSerial mySerial(D5,D6);//rx,tx
-VFD myVFD(mySerial); //D5,D6
+VFD myVFD(mySerial);
 
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808(); //for MCP9808
 
+//Initialize WiFi Connection -----
+void InitializeWiFi(){
+
+}
 
 //Initialize Temp Sensor Fnc. -----
 void InitializeTemp(){
@@ -57,74 +73,202 @@ float readTemp() {
   return f;
 }
 
+//fnc to display BTC price -----
+float fetchBtcPrice() {
+
+  // Connect to API
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return 00;
+  }
+
+  // We now create a URI for the request
+  String url = "/v1/bpi/currentprice.json";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return 00;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  String answer;
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    answer += line;
+  }
+
+  client.stop();
+  Serial.println();
+  Serial.println("closing connection");
+
+  // Process answer
+  Serial.println();
+  Serial.println("Answer: ");
+  Serial.println(answer);
+
+  // Convert to JSON
+  String jsonAnswer;
+  int jsonIndex;
+
+  for (int i = 0; i < answer.length(); i++) {
+    if (answer[i] == '{') {
+      jsonIndex = i;
+      break;
+    }
+  }
+
+  // Get JSON data
+  jsonAnswer = answer.substring(jsonIndex);
+  Serial.println();
+  Serial.println("JSON answer: ");
+  Serial.println(jsonAnswer);
+  jsonAnswer.trim();
+
+  // Get rate as float
+  int rateIndex = jsonAnswer.indexOf("rate_float");
+  String priceString = jsonAnswer.substring(rateIndex + 12, rateIndex + 18);
+  priceString.trim();
+  float price = priceString.toFloat();
+  return price;
+}
+
+//fnc to display ETH hashrate -----
+float fetchEthHash() {
+
+  // Connect to API
+  Serial.print("connecting to ");
+  Serial.println(hostEth);
+
+  // Use WiFiClient class to create TCP connections
+
+  WiFiClientSecure client;
+  const int httpPort = 443;
+  if (!client.connect(hostEth, httpPort)) {
+    Serial.println("connection failed");
+    return 00;
+  }
+
+  // We now create a URI for the request
+  String url = "/v1/eth/avghashratelimited/0x884e51352e7c68BfC9bA230f487be963a11de11B/1";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + hostEth + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return 00;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  String answer;
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    answer += line;
+  }
+
+  client.stop();
+  Serial.println();
+  Serial.println("closing connection");
+
+  // Process answer
+  Serial.println();
+  Serial.println("Answer: ");
+  Serial.println(answer);
+
+  // Convert to JSON
+  String jsonAnswer;
+  int jsonIndex;
+
+  for (int i = 0; i < answer.length(); i++) {
+    if (answer[i] == '{') {
+      jsonIndex = i;
+      break;
+    }
+  }
+
+  // Get JSON data
+  jsonAnswer = answer.substring(jsonIndex);
+  Serial.println();
+  Serial.println("JSON answer: ");
+  Serial.println(jsonAnswer);
+  jsonAnswer.trim();
+
+  // Get rate as float
+  int rateIndex = jsonAnswer.indexOf("data");
+  String priceString = jsonAnswer.substring(rateIndex + 6, rateIndex + 12);
+  priceString.trim();
+  float price = priceString.toFloat();
+  return price;
+}
+
 //Setup -----
 void setup() {
-
-  delay(4000);
   Serial.begin(115200);
-  //mySerial.begin(9600);
+  delay(10);
 
-  pinMode(D4,OUTPUT);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-  //connectWiFi();
+  WiFi.begin(ssid, password);
 
-  //delay(1000);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-  //together these Initialize the display
-  //mySerial.write('\x1B');
-  //mySerial.write('\x40');
+  Serial.println("");
+  Serial.println("WiFi connected");
+  myVFD.print("WiFi Connected: " + (String)ssid); //display on VFD
+  delay(4000);
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  delay(1000);
+  //InitializeWiFi(); //call to initialize wifi fnc.
 
-//  mySerial.write('\x0C'); //clear display
-  //mySerial.print("");
   myVFD.print("VFD Display code V1");
-
-
-  //commands to change brightness based on CD5220 Command Set
-  //mySerial.write('\x1B');
-  //mySerial.write('\x2A');
-  //mySerial.write('\x02'); //brightness select 1 <= n <= 4
-
-  myVFD.brightness(2);
-
-  delay(1000);
+  myVFD.brightness(1);
 
   InitializeTemp(); //Initialize temp sensor
 
-  //mySerial.write('\x0E');
-  //mySerial.write('\x0C');
-
 }
-
-
 
 //Loop -----
 void loop() {
 
-  delay(3000);
-
-  //mySerial.write('\x0C'); //clear display
-  //mySerial.write('\x0B'); //move cursor to home position
-  myVFD.clearHome();
   myVFD.print("Hacker HUD V1.0");
-  //mySerial.write('\x0A');
-  myVFD.print("test code by Coyt");
-
+  delay(2000);
+  myVFD.print("Current Temp: " + (String)readTemp()); //print current temp reading to VFD
   delay(3000);
-
-  //mySerial.write('\x0C');
-  //mySerial.write('\x0B');
-  myVFD.clearHome();
-  myVFD.print("HACK THE PLANET YAY");
-
+  myVFD.print("$" + (String)fetchBtcPrice() + " /BTC" ); //display BTC price
   delay(3000);
-  //mySerial.write('\x0C');
-  //mySerial.write('\x0B');
-  myVFD.clearHome();
-  myVFD.print("Current Temp: "); //print current temp reading to VFD
-  myVFD.print(readTemp());
+  myVFD.print("MH/s: " + (String)fetchEthHash() + " Hashrate" ); //display BTC price
+  delay(3000);
 
 }
-
-//Initialize Display Fnc. -----
