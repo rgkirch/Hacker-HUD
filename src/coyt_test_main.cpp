5c88273fad6c853b5126772273b94ba2a9c12022
coyt <coyt.barringer@Gmail.com>
2016-11-02 21:26:54 -0400

    dunno

--- src/coyt_test_main.cpp
+++ src/coyt_test_main.cpp
@@ -196,7 +196,73 @@ String readTime(){
 
   return jsonTime;
 }
+unsigned long webUnixTime()
+{
+    WiFiClient client;
+    unsigned long time = 0;
 
+    // Just choose any reasonably busy web server, the load is really low
+    if (client.connect("g.cn", 80))
+    {
+        // Make an HTTP 1.1 request which is missing a Host: header
+        // compliant servers are required to answer with an error that includes
+        // a Date: header.
+        client.print(F("GET / HTTP/1.1 \r\n\r\n"));
+
+        char buf[5]; // temporary buffer for characters
+        client.setTimeout(5000);
+        if(client.find((char *)"\r\nDate: ") && client.readBytes(buf, 5) == 5) // look for Date: header; discard
+        {
+            unsigned day = client.parseInt(); // day
+            client.readBytes(buf, 1); // discard
+            client.readBytes(buf, 3); // month
+            int year = client.parseInt(); // year
+            byte hour = client.parseInt(); // hour
+            byte minute = client.parseInt(); // minute
+            byte second = client.parseInt(); // second
+            int daysInPrevMonths;
+            switch (buf[0])
+            {
+                case 'F': daysInPrevMonths = 31; break; // Feb
+                case 'S': daysInPrevMonths = 243; break; // Sep
+                case 'O': daysInPrevMonths = 273; break; // Oct
+                case 'N': daysInPrevMonths = 304; break; // Nov
+                case 'D': daysInPrevMonths = 334; break; // Dec
+                default:
+                if (buf[0] == 'J' && buf[1] == 'a')
+        daysInPrevMonths = 0;		// Jan
+                else if (buf[0] == 'A' && buf[1] == 'p')
+        daysInPrevMonths = 90;		// Apr
+                else switch (buf[2])
+                {
+                case 'r': daysInPrevMonths =    59; break; // Mar
+                case 'y': daysInPrevMonths = 120; break; // May
+                case 'n': daysInPrevMonths = 151; break; // Jun
+                case 'l': daysInPrevMonths = 181; break; // Jul
+                default: // add a default label here to avoid compiler warning
+                case 'g': daysInPrevMonths = 212; break; // Aug
+                }
+            }
+
+            // This code will not work after February 2100
+            // because it does not account for 2100 not being a leap year and because
+            // we use the day variable as accumulator, which would overflow in 2149
+            day += (year - 1970) * 365;	// days from 1970 to the whole past year
+            day += (year - 1969) >> 2;	// plus one day per leap year
+            day += daysInPrevMonths;	// plus days for previous months this year
+            if (daysInPrevMonths >= 59	// if we are past February
+                    && ((year & 3) == 0))	// and this is a leap year
+                day += 1;			// add one day
+            // Remove today, add hours, minutes and seconds this month
+            time = (((day-1ul) * 24 + hour) * 60 + minute) * 60 + second;
+        }
+    }
+    delay(10);
+    client.flush();
+    client.stop();
+
+    return time;
+}
 //Setup -----
 void setup() {
   Serial.begin(115200);
@@ -214,6 +280,7 @@ void setup() {
 void loop() {
 
   myVFD.print("  Hacker HUD V1.0  ");
+  Serial.println(webUnixTime());
   delay(2000);
   myVFD.print("Current Temp: " + (String)readTemp()); //print current temp reading to VFD
   delay(2000);
