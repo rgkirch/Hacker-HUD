3ff27a6af0ee8568a9922f47034c5de44db72676
richie <rgkirch@gmail.com>
2017-03-13 15:49:51 -0400

    moved espinfo to a file

---
+++ src/espInfo.hpp
@@ -0,0 +1,17 @@
+void printEspInfo()
+{
+    Serial.println(ESP.getVcc());
+    Serial.println(ESP.getFreeHeap());
+    Serial.println(ESP.getChipId());
+    Serial.println(ESP.getSdkVersion());
+    Serial.println(ESP.getCoreVersion());
+    Serial.println(ESP.getBootVersion());
+    Serial.println(ESP.getBootMode());
+    Serial.println(ESP.getCpuFreqMHz());
+    Serial.println(ESP.getFlashChipId());
+    Serial.println(ESP.getFlashChipRealSize()); //gets the actual chip size based on the flash id
+    Serial.println(ESP.getFlashChipSize()); //gets the size of the flash as set by the compiler
+    Serial.println(ESP.getFlashChipSpeed());
+    Serial.println(ESP.getFlashChipMode());
+    Serial.println(ESP.getFlashChipSizeByChipId());
+}
--- src/main.cpp
+++ src/main.cpp
@@ -35,26 +35,6 @@ void *memchr(const void *s, int c, size_t n)
 std::unique_ptr<VFD> myVFD;
 struct Site coindesk {.host = "api.coindesk.com", .path = "v1/bpi/currentprice.json", .port = httpsPort};
 
-void printEspInfo()
-{
-    Serial.println(ESP.getVcc());
-    Serial.println(ESP.getFreeHeap());
-    Serial.println(ESP.getChipId());
-    Serial.println(ESP.getSdkVersion());
-    Serial.println(ESP.getCoreVersion());
-    Serial.println(ESP.getBootVersion());
-    Serial.println(ESP.getBootMode());
-    Serial.println(ESP.getCpuFreqMHz());
-    Serial.println(ESP.getFlashChipId());
-    Serial.println(ESP.getFlashChipRealSize()); //gets the actual chip size based on the flash id
-    Serial.println(ESP.getFlashChipSize()); //gets the size of the flash as set by the compiler
-    Serial.println(ESP.getFlashChipSpeed());
-    Serial.println(ESP.getFlashChipMode());
-    Serial.println(ESP.getFlashChipSizeByChipId());
-}
-
-char fox[100] = "the quick brown fox jumps over the lazy dog";
-
 void setup()
 {
     Serial.begin(115200);
