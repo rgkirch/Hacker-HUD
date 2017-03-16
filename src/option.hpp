847611cb9b2103bc624a714ae18b06c1b002de39
richie <rgkirch@gmail.com>
2017-03-16 12:01:01 -0400

    better option

--- src/option.hpp
+++ src/option.hpp
@@ -1,25 +1,20 @@
 template <typename T>
 class Option {
 public:
-    Option(T d) : data(d), err(false) {};
+    Option(T d) : data(d), empty(false) {}; // check not null
+    Option() : empty(true) {};
 //    Option(Option &o) {
-//        if (o.isError()) {
-//            err = true;
+//        if (o.isEmpty()) {
+//            empty = true;
 //        } else {
 //            data = o.data;
-//            err = false;
+//            empty = false;
 //        }
 //    };
-    bool isError() { return this->err; };
-    template <typename E>
-    T getOrElse(E e) { if(not this->isError()) return data; else return e; };
-    static Option error() { return new Option(); };
-    static Option error(std::string str) { return new Option(); }; // placeholder
-//    static Option error(std::string str) { return new Option(str); }; // make new option, set string, return option
-    std::string errorString;
+    bool isEmpty() { return this->empty; };
+    T getOrElse(T e) { if(not this->isEmpty()) return data; else return e; };
 private:
-    Option() : err(true) {};
-//    Option(std::string str) : err(true), errorString(str) {}; // delete this constructor if conflict with template constructor
+//    Option() : empty(true) {};
     T data;
-    bool err;
+    bool empty;
 };
