e390dc517a398c508fa5e68b2715758e9a83a6b7
richard <rgkirch@gmail.com>
2017-06-01 19:05:02 -0400

    refactored all with macros

--- test/displayTest.cpp
+++ test/displayTest.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <string>
+//#include <string>
 #include "display.h"
 
 using ::testing::A;
@@ -18,62 +18,24 @@ public:
     MOCK_METHOD1(print, size_t(const char *));
 };
 
-#define TESTUPPERLINEONE(test_case_name, test_case, display_width, input_string, match_string) \
+#define TESTUPPERLINE(test_case_name, test_case, display_width, expected, ...) \
 TEST(test_case_name, test_case) { \
     MockAbstractSerial serial(1, 2); \
     VFD vfd(display_width, 2, &serial); \
-    std::string str = input_string; \
     EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x1B\x51\x41")))).Times(1); \
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq(match_string)))).Times(1); \
+    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq(expected)))).Times(1); \
     EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x0D")))).Times(1); \
-    vfd.setUpperLine(str); \
+    vfd.setUpperLine(__VA_ARGS__); \
 }
 
-TESTUPPERLINEONE(vfd, setUpperLine, 20, "hello", "hello               ")
-TESTUPPERLINEONE(vfd, setUpperLineCalledWithLongArgument, 18, "one two three four five six", "one two three four")
-TESTUPPERLINEONE(vfd, setUpperLineEmptyString, 7, "", "       ")
-TESTUPPERLINEONE(vfd, setLowerLine, 20, "hello", "hello               ")
-TESTUPPERLINEONE(vfd, setLowerLineCalledWithLongArgument, 18, "one two three four five six", "one two three four")
-TESTUPPERLINEONE(vfd, setLowerLineEmptyString, 7, "", "       ")
-
-//TESTUPPERLINETWO(vfd, setUpperLineTwoStrings, 20, "temp", "45C", "temp             45C")
-
-TEST(vfd, setUpperLineTwoStringsTooSmallForRightMostAndSomeOfLeft) {
-    MockAbstractSerial serial(5, 6);
-    VFD vfd(8, 2, &serial);
-    std::string temp = "temperature", deg = "45C";
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x1B\x51\x41")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("temperat")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x0D")))).Times(1);
-    vfd.setUpperLine(temp, deg);
-}
-
-TEST(vfd, setUpperLineTwoStringsTooSmallForRightString) {
-    MockAbstractSerial serial(5, 6);
-    VFD vfd(11, 2, &serial);
-    std::string temp = "temperature", deg = "45C";
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x1B\x51\x41")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("temperature")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x0D")))).Times(1);
-    vfd.setUpperLine(temp, deg);
-}
-
-TEST(vfd, setUpperLineTwoStringsRightStringGetsChopped) {
-    MockAbstractSerial serial(5, 6);
-    VFD vfd(12, 2, &serial);
-    std::string temp = "temperature", deg = "45C";
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x1B\x51\x41")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("temperature4")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x0D")))).Times(1);
-    vfd.setUpperLine(temp, deg);
-}
-
-TEST(vfd, setLowerLineTwoStringsLeftChopOneChar) {
-    MockAbstractSerial serial(5, 6);
-    VFD vfd(10, 2, &serial);
-    std::string temp = "temperature", deg = "45C";
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x1B\x51\x42")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("temperatur")))).Times(1);
-    EXPECT_CALL(serial, print(Matcher<const char*>(StrEq("\x0D")))).Times(1);
-    vfd.setLowerLine(temp, deg);
-}
+TESTUPPERLINE(vfd, setUpperLine, 20, "hello               ", "hello")
+TESTUPPERLINE(vfd, setUpperLineCalledWithLongArgument, 18, "one two three four", "one two three four five six")
+TESTUPPERLINE(vfd, setUpperLineEmptyString, 7, "       ", "")
+TESTUPPERLINE(vfd, setLowerLine, 20, "hello               ", "hello")
+TESTUPPERLINE(vfd, setLowerLineCalledWithLongArgument, 18, "one two three four", "one two three four five six")
+TESTUPPERLINE(vfd, setLowerLineEmptyString, 7, "       ", "")
+TESTUPPERLINE(vfd, setUpperLineTwoStrings, 20, "temp             45C", "temp", "45C")
+TESTUPPERLINE(vfd, setUpperLineTwoStringsTooSmallForRightMostAndSomeOfLeft, 8, "temperat", "temperature", "45C")
+TESTUPPERLINE(vfd, setUpperLineTwoStringsTooSmallForRightString, 11, "temperature", "temperature", "45C")
+TESTUPPERLINE(vfd, setUpperLineTwoStringsRightStringGetsChopped, 12, "temperature4", "temperature", "45C")
+TESTUPPERLINE(vfd, setLowerLineTwoStringsLeftChopOneChar, 10, "temperatur", "temperature", "45C")
