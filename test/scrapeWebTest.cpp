8c5fc12eb577a494ef05a6f8a30e13ade7ff3705
richard <rgkirch@gmail.com>
2017-06-03 18:00:05 -0400

    added a test for scrapeWeb!

--- test/scrapeWebTest.cpp
+++ test/scrapeWebTest.cpp
@@ -2,6 +2,19 @@
 #include "gmock/gmock.h"
 #include "scrapeWeb.hpp"
 
+using ::testing::A;
+using ::testing::AtLeast;
+using ::testing::Invoke;
+using ::testing::Matcher;
+using ::testing::Return;
+using ::testing::StrEq;
+using ::testing::_;
+
+TEST(makeGetRequest, basic) {
+    std::string str = makeGetRequest("api.coindesk.com", "v1/bpi/currentprice.json");
+    EXPECT_STREQ(str.c_str(), "GET /v1/bpi/currentprice.json HTTP/1.1\r\nHost: api.coindesk.com\r\nConnection: close\r\n\r\n");
+}
+
 TEST(sanity, one) {
     ASSERT_EQ(1, 1);
 }
