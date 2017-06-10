595269954e1a4c8ec6d82c0d9e4be1fbd292805e
richard <rgkirch@gmail.com>
2017-06-10 11:59:22 -0400

    deleted the wait on client->available
    wow, i forgot to return the client pointer in that one function and that caused a lot of trouble

--- pio/src/scrapeWeb.hpp
+++ pio/src/scrapeWeb.hpp
@@ -9,6 +9,8 @@
 
 #include "option.hpp"
 #include "site.hpp"
+
+//#include "myConnection.hpp"
 //#ifdef min
 //#undef min
 //#endif
@@ -19,8 +21,33 @@
 //#define min(a,b) ((a)<(b)?(a):(b))
 //#define max(a,b) ((a)>(b)?(a):(b))
 
+//#define DEBUGPRINT
+#ifdef DEBUGPRINT
+#define LOG(x) do{Serial.println(x);}while(0)
+#else
+#define LOG(x)
+#endif
+
+Option<std::string> emptyOption;
+
+std::string makeGetRequest(std::string host, std::string path)
+{
+    LOG("make get request string overload");
+    std::string request;
+    std::string get = {"GET /"};
+    std::string http = {" HTTP/1.1\r\nHost: "};
+    std::string close = {"\r\nConnection: close\r\n\r\n"};
+    request.append(get);
+    request.append(path);
+    request.append(http);
+    request.append(host);
+    request.append(close);
+    return request;
+}
+
 std::string makeGetRequest(const char *host, const char *path)
 {
+    LOG("make get request pointer overload");
     std::string request;
     std::string get = {"GET /"};
     std::string http = {" HTTP/1.1\r\nHost: "};
@@ -32,45 +59,56 @@ std::string makeGetRequest(const char *host, const char *path)
     request.append(close);
     return request;
 }
-Option<std::string> downloadSiteData(int port, const char *host, const char *path) {
-    Option<std::string> emptyOption;
+
+WiFiClient* createWificlient(int port, const char *host, const char *path) {
+    LOG("create wifi client");
     WiFiClient* client;
-    if(port == httpsPort)
-    {
-        client = new WiFiClientSecure;
+    if(port == httpsPort) {
+        client = new WiFiClientSecure();
     } else if (port == httpPort){
-        client = new WiFiClient;
-    } else return emptyOption; //"port incorrect"
-    if (client == NULL or client == nullptr) return emptyOption; //"couldn't make client"
+        client = new WiFiClient();
+    } else return nullptr; //"port incorrect"
 
     if (not client->connect(host, port)) {
-        return emptyOption; //"client connect failed"
+        return nullptr; //"client connect failed"
     }
     if (not client->connected()) {
-        return emptyOption; //"client not connected?!?!"
+        return nullptr; //"client not connected?!?!"
     }
     client->print(makeGetRequest(host, path).c_str());
+    LOG("exit create wifi client");
+    return client;
+}
 
-    unsigned long timeout = millis();
-    while (!client->available()) {
-        yield();
-        if (millis() - timeout > 5000) {
-            client->stop();
-            return emptyOption; //"client timed out"
-        }
+
+Option<std::string> downloadSiteData(int port, const char *host, const char *path) {
+    LOG("download site data");
+    WiFiClient *client = createWificlient(port, host, path);
+    if(client == nullptr) {
+        LOG("client returned from create wificlient is null");
+        return emptyOption;
     }
-
+    int num = 0;
     std::string data;
     while(client->connected())
     {
+        LOG("read data from client");
         data.append(client->readString().c_str());
+        LOG("after read data from client");
+        num++;
     }
+    if(num > 1) Serial.println("reading more data from string");
+    LOG("find the left brace");
     auto i = data.find('{');
     if (i == data.npos) {
+        LOG("didn't find the left brace");
         i = 0;
     }
+    LOG("stop client");
     client->stop();
+    LOG("delete client");
     delete client;
+    LOG("exit download site data");
     return data.substr(i);
 }
 
