40e2a3063be74cc42703dfa1fc09ec37c198d530
richie <rgkirch@gmail.com>
2017-06-13 18:51:01 -0400

    added comments for clarification

--- test/myConcreteConnectionTest.cpp
+++ test/myConcreteConnectionTest.cpp
@@ -26,15 +26,15 @@ TEST(downloadData, basic) {
     int port = 80;
     const char *host = "api.coindesk.com";
     const char *path = "v1/bpi/currentprice.json";
-    MockMyClient client;
-    MyConcreteConnection connection(&client, host, path);
-    std::string str = makeGetRequest(host, path);
+    MockMyClient client; // connection happens through client
+    MyConcreteConnection connection(&client, host, path); // connection prints text to client and reads from client
+    std::string str = makeGetRequest(host, path); // the text that connection will print to client
 //    std::string str = "hi";
     std::string json = "one two three {four five six}";
-    EXPECT_CALL(client, print(Matcher<const char*>(StrEq(str.c_str())))).Times(1);
-    ON_CALL(client, read()).WillByDefault(Return(InvokeWithoutArgs([&json]()->int {
-        static auto it = json.begin();
-        return (int)(*(it++));
+    EXPECT_CALL(client, print(Matcher<const char*>(StrEq(str.c_str())))).Times(1); // the client's print should be called with str
+    ON_CALL(client, read()).WillByDefault(Return(InvokeWithoutArgs([&json]()->int { // when the connection calls the clients read, it should
+        static auto it = json.begin();t 
+        return (int)(*(it++)); // return the text in json
     })));
 //    ON_CALL(client, read()).WillByDefault(Return(8));
     std::string data = connection.read();
