8fef0b376c031581e07f15a64273f5b974e55072
richie <rgkirch@gmail.com>
2017-06-13 20:20:19 -0400

    umm, use constructor?

--- test/myConcreteConnectionTest.cpp
+++ test/myConcreteConnectionTest.cpp
@@ -14,7 +14,7 @@ using ::testing::_;
 
 class MockMyClient: public MyClient {
 public:
-//    MockMyClient(uint16_t port) {};
+    MockMyClient(uint16_t port) {};
     MOCK_METHOD2(connect, int(uint16_t port, const char *host));
     MOCK_METHOD1(print, size_t(const char[]));
     MOCK_METHOD0(connected, uint8_t());
@@ -26,14 +26,14 @@ TEST(downloadData, basic) {
     int port = 80;
     const char *host = "api.coindesk.com";
     const char *path = "v1/bpi/currentprice.json";
-    MockMyClient client; // connection happens through client
+    MockMyClient client(port); // connection happens through client
     MyConcreteConnection connection(&client, host, path); // connection prints text to client and reads from client
     std::string str = makeGetRequest(host, path); // the text that connection will print to client
 //    std::string str = "hi";
     std::string json = "one two three {four five six}";
     EXPECT_CALL(client, print(Matcher<const char*>(StrEq(str.c_str())))).Times(1); // the client's print should be called with str
     ON_CALL(client, read()).WillByDefault(Return(InvokeWithoutArgs([&json]()->int { // when the connection calls the clients read, it should
-        static auto it = json.begin();t 
+        static auto it = json.begin();
         return (int)(*(it++)); // return the text in json
     })));
 //    ON_CALL(client, read()).WillByDefault(Return(8));
