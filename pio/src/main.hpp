6e87e5a9e23ed5796fefbcf2807a448a3885b97f
richie <rgkirch@gmail.com>
2017-12-29 10:39:53 -0500

    main.hpp

---
+++ pio/src/main.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <functional>
+
+using std::string;
+using std::vector;
+using std::function;
+
+vector<function<string()>> frames;
+int getWhichFrame(int numFrames, long timePerFrame, long currentTime) {
+    int period = numFrames * timePerFrame;
+    if (currentTime < 0) currentTime += period * (currentTime / period + 1);
+    int currentFrame = (currentTime % (numFrames * timePerFrame)) / timePerFrame;
+    return currentFrame;
+}
